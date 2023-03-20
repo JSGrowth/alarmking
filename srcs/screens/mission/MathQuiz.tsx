5438bd529376f50393186f576d0d1d9337364b2c
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2023-03-20 12:02:23 +0900

    not working: color animation based on if the pressable is true or not

--- srcs/screens/mission/MathQuiz.tsx
+++ srcs/screens/mission/MathQuiz.tsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useLayoutEffect, useRef, useState} from 'react';
-import {Animated, FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Animated, SafeAreaView, StyleSheet, View} from 'react-native';
 import theme from '@common/theme';
 import OptionItem from './components/OptionItem';
 
 export default function MathQuiz() {
   const makeQuiz = () => {
-    const a = Math.floor(Math.random() * 90) + 1;
-    const b = Math.floor(Math.random() * 90) + 1;
+    const a = Math.floor(Math.random() * 9) + 1;
+    const b = Math.floor(Math.random() * 9) + 1;
     const c = Math.floor(Math.random() * 10) + 3;
     const op = [' + ', ' - '][Math.floor(Math.random() * 2)];
     return a + op + b + ' * ' + c;
@@ -15,16 +15,20 @@ export default function MathQuiz() {
   const fadeIn = useRef(new Animated.Value(0)).current;
   const translationy = useRef(new Animated.Value(0)).current;
   const quizTimeOver = useRef(new Animated.Value(1)).current;
-  const quiz = makeQuiz();
-  const answer = parseInt(eval(quiz));
-  const optionData: number[] = [];
-  for (let i = 0; i < 3; i++)
-    optionData.push(
-      answer +
-        Math.ceil(Math.random() * 20) * (Math.round(Math.random()) ? 1 : -1),
-    );
-  optionData.push(answer);
-  optionData.sort();
+  let quiz = makeQuiz();
+  let answer = parseInt(eval(quiz));
+  const makeOptions = (answer: number) => {
+    const returnArr = [];
+    for (let i = 0; i < 3; i++)
+      returnArr.push(
+        answer +
+          Math.ceil(Math.random() * 20) * (Math.round(Math.random()) ? 1 : -1),
+      );
+    returnArr.push(answer);
+    returnArr.sort();
+    return returnArr;
+  };
+  let optionData: number[] = makeOptions(answer);
   const animatedView = (
     toValue: number,
     animatedRef: Animated.Value,
@@ -48,6 +52,11 @@ export default function MathQuiz() {
     ]).start();
   }, []);
 
+  const hanldePress = () => {
+    /* turn off the alarm and navigate to main page */
+    console.log('navigate to main page');
+  };
+
   return (
     <SafeAreaView style={styles.safeareaview}>
       <View style={[styles.timeview]}>
@@ -62,8 +71,12 @@ export default function MathQuiz() {
           {quiz.replace('*', 'x')}
         </Animated.Text>
       </View>
-      <Animated.View style={{flex: 1, opacity: quizTimeOver}}>
-        <FlatList
+      <Animated.View
+        style={{
+          flex: 1,
+          opacity: quizTimeOver,
+        }}>
+        <Animated.FlatList
           data={optionData}
           numColumns={2}
           keyExtractor={item => item.toString()}
@@ -72,7 +85,7 @@ export default function MathQuiz() {
             <OptionItem
               value={item}
               answer={answer}
-              handlePress={(item, answer) => item === answer}
+              handlePress={hanldePress}
             />
           )}
         />
--- srcs/screens/mission/components/OptionItem.tsx
+++ srcs/screens/mission/components/OptionItem.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import theme, {fontSize} from '@common/theme';
 import {Animated, Pressable, StyleSheet, Text, View} from 'react-native';
 
 type optionPropsType = {
   value: number;
   answer: number;
-  handlePress: (answer: number, value: number) => boolean;
+  handlePress: () => void;
 };
 
-export default function OptionItem({value, handlePress}: optionPropsType) {
+export default function OptionItem({
+  value,
+  answer,
+  handlePress,
+}: optionPropsType) {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const handleAnswer = (
+    value: number,
+    answer: number,
+    callback: () => void,
+  ) => {
+    let toVal = 0;
+    value === answer ? (toVal = 1) : (toVal = -1);
+    Animated.timing(animatedValue, {
+      toValue: toVal,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+    callback();
+  };
+  const interpolatedColor = animatedValue.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [color.error, color.text_grey, color.text_primary],
+    // outputRange: [0, 1, 0],
+  });
+
   return (
-    <Animated.View style={[styles.view]}>
-      <Pressable onPress={() => handlePress} style={[styles.button]}>
+    <Animated.View style={[styles.view, {borderColor: interpolatedColor}]}>
+      {/*<Animated.View style={[styles.view, {opacity: interpolatedColor}]}> */}
+      <Pressable
+        onPress={() => handleAnswer(value, answer, handlePress)}
+        style={[styles.button]}>
         <Animated.Text style={[styles.text]} children={`${value}`} />
       </Pressable>
     </Animated.View>
@@ -23,7 +51,7 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     borderStyle: 'solid',
-    borderColor: color.text_grey,
+    // borderColor: color.text_grey,
     borderWidth: 2,
     borderRadius: 30,
     marginHorizontal: 16,
